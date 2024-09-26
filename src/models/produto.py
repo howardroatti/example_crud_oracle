9e55655e948383d2c9868bd47ad6c686a9608d34
Pierry Jonny <108902774+314erry@users.noreply.github.com>
2024-09-26 12:51:11 -0300

    Update produto.py

--- src/models/produto.py
+++ src/models/produto.py
@@ -1,19 +1,35 @@
-class Categoria:
-    def __init__(self, id_categoria:int=None, nome_categoria:str=None):
-        self.set_id_categoria(id_categoria)
-        self.set_nome_categoria(nome_categoria)
+from models.categoria import Categoria
 
-    def set_id_categoria(self, id_categoria:int):
-        self.id_categoria = id_categoria
+class Produto:
+    def __init__(self, id_produto:int=None, nome_produto:str=None, preco:float=None, categoria:Categoria=None):
+        self.set_id_produto(id_produto)
+        self.set_nome_produto(nome_produto)
+        self.set_preco(preco)
+        self.set_categoria(categoria)
 
-    def set_nome_categoria(self, nome_categoria:str):
-        self.nome_categoria = nome_categoria
+    def set_id_produto(self, id_produto:int):
+        self.id_produto = id_produto
 
-    def get_id_categoria(self) -> int:
-        return self.id_categoria
+    def set_nome_produto(self, nome_produto:str):
+        self.nome_produto = nome_produto
 
-    def get_nome_categoria(self) -> str:
-        return self.nome_categoria
+    def set_preco(self, preco:float):
+        self.preco = preco
+
+    def set_categoria(self, categoria:Categoria):
+        self.categoria = categoria
+
+    def get_id_produto(self) -> int:
+        return self.id_produto
+
+    def get_nome_produto(self) -> str:
+        return self.nome_produto
+
+    def get_preco(self) -> float:
+        return self.preco
+
+    def get_categoria(self) -> Categoria:
+        return self.categoria
 
     def to_string(self) -> str:
-        return f"ID Categoria: {self.get_id_categoria()} | Nome Categoria: {self.get_nome_categoria()}"
+        return f"ID Produto: {self.get_id_produto()} | Nome Produto: {self.get_nome_produto()} | Preço: {self.get_preco()} | Categoria: {self.get_categoria().get_nome_categoria()}"
